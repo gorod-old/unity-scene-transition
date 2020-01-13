1a150c108fda106b9d649f2720970767ee665e5d
gorod-old <gorod1974@hotmail.com>
2020-01-13 17:19:00 +0300

    lesson loading

---
+++ Scene-transition/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Start()
+    {
+        animator.SetInteger("start", 1);
+    }
+
+    public void NextLevel()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        animator.SetInteger("start", 2);
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex == 0 ? 1 : 0);
+    }
+}
